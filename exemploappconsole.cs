33d64000872b720cfe98d1f40c2e6e0e4504969d
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-03-10 17:24:12 -0300

    Aula 03 C#.
    Hello World!

---
+++ exemploappconsole.cs
@@ -0,0 +1,11 @@
+namespace ExemploAppConsole 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
