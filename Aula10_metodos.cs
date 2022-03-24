763ac9e4b361e4ce164d27368c74154ddd2a4eeb
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-03-23 23:20:06 -0300

    Métodos.
    Utilizando métodos.

---
+++ Aula10_metodos.cs
@@ -0,0 +1,18 @@
+namespace Aula10
+{
+    public class Calculos
+    {
+        //Método
+        public async int Somar(int a, int b)
+        {
+            int resultado = a + b;
+            return resultado;
+        }
+        //Método
+        public async int Subtrair(int a, int b)
+        {
+            return a - b;
+            
+        }
+    }
+}
